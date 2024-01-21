22aa8ae9873730790ac38c010097b323b5232c05
SURAJ-K-GUPTA <116446956+SURAJ-K-GUPTA@users.noreply.github.com>
2024-01-21 20:37:19 +0530

    filmyverse completed

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { createContext, useState } from 'react';
+import './App.css';
+import AddMovie from './components/AddMovie';
+import Cards from './components/Cards';
+import Details from './components/Details';
+import Header from "./components/Header";
+import {Route, Routes} from 'react-router-dom'
+import Login from './components/Login';
+import Signup from './components/Signup';
+
+
+const Appstate = createContext();
+function App() {
+  const[login, setLogin] = useState(false);
+  const [userName, setUserName] = useState("")
+
+
+  return (
+  <Appstate.Provider value={{login, userName, setLogin, setUserName}}>
+      <div className="App relative">
+        <Header />
+        <Routes>
+          <Route path='/' element={<Cards />} />
+          <Route path='/addmovie' element={<AddMovie />} />
+          <Route path='/details/:id' element={<Details />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </div>
+  </Appstate.Provider>
+  );
+}
+
+export default App;
+export {Appstate}
